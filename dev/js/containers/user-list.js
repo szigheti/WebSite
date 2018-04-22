eaae3c6094058fdabce882ee36d409e5c879b607
szigheti <szigheti@users.noreply.github.com>
2018-04-22 11:50:15 +0300

    Add files via upload

---
+++ dev/js/containers/user-list.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {selectUser} from '../actions/index'
+
+
+class UserList extends Component {
+
+    createListItems() {
+        return this.props.users.map((user) => {
+            return (
+                <li
+                    key={user.id}
+                    onClick={() => this.props.selectUser(user)}
+                >
+                    {user.first} {user.last}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.createListItems()}
+            </ul>
+        );
+    }
+}
+
+    function mapStateToProps(state) {
+        return {
+            users: state.users
+        };
+    }
+
+
+
+function matchDispatchToProps(dispatch){
+    return bindActionCreators({selectUser: selectUser}, dispatch);
+}
+
+export default connect (mapStateToProps, matchDispatchToProps)(UserList);
+
+
+
+
+//    renderList() {
+//        return this.props.users.map((user) => {
+//            return (
+//                <li
+//                    key={user.id}
+//                    onClick={() => this.props.selectUser(user)}
+//                >
+//                    {user.first} {user.last}
+//                </li>
+//            );
+//        });
+//    }
+
+
+
+//    render() {
+//        return (
+//            <ul>
+//                {this.renderList()}
+//            </ul>
+//        );
+//    }
+
+// Get apps state and pass it as props to UserList
+//      > whenever state changes, the UserList will automatically re-render
+//function mapStateToProps(state) {
+//    return {
+//        users: state.users
+//    };
+//}
+
+// Get actions and pass them as props to to UserList
+//      > now UserList has this.props.selectUser
+//function matchDispatchToProps(dispatch){
+//    return bindActionCreators({selectUser: selectUser}, dispatch);
+//}
+
+// We don't want to return the plain UserList (component) anymore, we want to return the smart Container
+//      > UserList is now aware of state and actions
+//export default connect(mapStateToProps, matchDispatchToProps)(UserList);
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,33 @@
+/*
+ * The users reducer will always return an array of users no matter what
+ * You need to return something, so if there are no users then just return an empty array
+ * */
+
+export default function () {
+    return [
+        {
+            id: 1,
+            first: "Bucky",
+            last: "Roberts",
+            age: 71,
+            description: "Bucky is a React developer and YouTuber",
+            thumbnail: "https://www.google.co.il/imgres?imgurl=http%3A%2F%2Fstories4kid.com%2F313667_196.jpg&imgrefurl=http%3A%2F%2Fstories4kid.com%2FPAGE69.asp&docid=Txit7P3ly0zVXM&tbnid=nQcXzKUPO2MVpM%3A&vet=10ahUKEwiV84Xa5MDaAhVEGZoKHcefAOwQMwgxKAEwAQ..i&w=800&h=600&bih=974&biw=1920&q=%D7%AA%D7%9E%D7%95%D7%A0%D7%95%D7%AA&ved=0ahUKEwiV84Xa5MDaAhVEGZoKHcefAOwQMwgxKAEwAQ&iact=mrc&uact=8"
+        },
+        {
+            id: 2,
+            first: "Joby",
+            last: "Wasilenko",
+            age: 27,
+            description: "Joby loves the Packers, cheese, and turtles.",
+            thumbnail: "https://www.google.co.il/imgres?imgurl=http%3A%2F%2Ftools.gaiamobile.netdna-cdn.com%2Fkidstv%2Fmedia%2F5%2F52921e8d0c8f912d5b88f966b0257121.jpg&imgrefurl=http%3A%2F%2Fwww.kidstv.co.il%2FLOGI%2Farticle%2FA1940&docid=P0MHGJyYL2mKqM&tbnid=yriuwnZWIioxBM%3A&vet=10ahUKEwiV84Xa5MDaAhVEGZoKHcefAOwQMwg2KAYwBg..i&w=600&h=310&bih=974&biw=1920&q=%D7%AA%D7%9E%D7%95%D7%A0%D7%95%D7%AA&ved=0ahUKEwiV84Xa5MDaAhVEGZoKHcefAOwQMwg2KAYwBg&iact=mrc&uact=8"
+        },
+        {
+            id: 3,
+            first: "Madison",
+            last: "Williams",
+            age: 24,
+            description: "Madi likes her dog but it is really annoying.",
+            thumbnail: "https://www.google.co.il/search?q=%D7%AA%D7%9E%D7%95%D7%A0%D7%95%D7%AA+%D7%A7%D7%98%D7%A0%D7%95%D7%AA&tbm=isch&source=iu&ictx=1&fir=6Vwj-4lzj__NKM%253A%252C_62xkqp9pjVDdM%252C_&usg=__5s3je63I7QAUDJ5ViENmyA1QFZg%3D&sa=X&ved=0ahUKEwiU7K6g5cDaAhXGx6YKHWH2DbkQ9QEIOjAJ#imgrc=6Vwj-4lzj__NKM:"
+        }
+    ]
+}
